fc732ec324a9deab1e3db3ed78c663e2b50c2b2e
Joel <joelmathew.v2@gmail.com>
2022-10-09 22:10:43 -0400

    Submission 1 of GitHub exercise 4.

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -10,4 +10,59 @@
  * Look at DomesticatableTrader.java for an example.
  */
 
+import java.util.ArrayList;
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable> {
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int mon) {
+        super(mon);
+    }
+
+    public void addToWishlist(Drivable item) {
+        super.addToWishlist(item);
+    }
+
+    public int getSellingPrice(Drivable item) {
+        if (item instanceof Tradable) {
+            return super.getSellingPrice(item) + item.getMaxSpeed();
+        }
+        else {
+            return Tradable.MISSING_PRICE;
+        }
+    }
+
+    /**
+     * Exchange money from other to this Trader according to the price of item,
+     * if other has enough money. Otherwise, returns False.
+     *
+     * @return True if the exchange was completed.
+     */
+    public boolean exchangeMoney(Trader<Drivable> other, Drivable item) {
+        return super.exchangeMoney(other, item);
+    }
+
+    /**
+     * Attempt to sell all items that are in both this Trader's inventory
+     * and in other's wishlist.
+     *
+     * @return True iff at least one item was sold from this Trader to other
+     */
+    public boolean sellTo(Trader<Drivable> other) {
+        return super.sellTo(other);
+    }
+
+    /**
+     * Buy items from other.
+     *
+     * @return True iff at least one item was bought from other.
+     */
+    public boolean buyFrom(Trader<Drivable> other) {
+        return super.buyFrom(other);
+    }
+
+
+}
